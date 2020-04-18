23393d7a7b3b764fdbdea4cb629cd336dfe15677
user <user@example.com>
2020-04-18 13:40:20 -0500

    Testing planet class.

--- planet_demo/main.py
+++ planet_demo/main.py
@@ -12,19 +12,19 @@ class ColorMap(object):
       assert rect.width == self.SHEET_SIZE
       assert rect.height == self.SHEET_SIZE
       get_rid_of_alpha = ~(0xff << sheet.get_shifts()[3])
-      self.colors = np.bitwise_and(subsurface.array2d(sheet.subsurface(rect)),get_rid_of_alpha)
+      self.colors = np.bitwise_and(surfarray.array2d(sheet.subsurface(rect)),get_rid_of_alpha)
 
 class BiomeMap(object):
    def __init__(self,sheet,rect):
       subsurface = sheet.subsurface(rect)
       data = surfarray.array2d(subsurface)
       self.shifts = subsurface.get_shifts()
-      self.elevation = self.isolate_and_divide(data,self.shifts[0])
-      self.temperature = self.isolate_and_divide(data,self.shifts[1])
-      self.alpha = np.bitwise_and(data,0xff << self.shifts[3])
+      self.elevation = self.isolate_and_divide(data,self.shifts[0]).astype(dtype=np.uint32,casting="unsafe",copy=False)
+      self.temperature = self.isolate_and_divide(data,self.shifts[1]).astype(dtype=np.uint32,casting="unsafe",copy=False)
+      self.alpha = np.bitwise_and(data,0xff << self.shifts[3]).astype(dtype=np.uint32,casting="unsafe",copy=False)
       self.scratch = data.copy()
-      self.altered_temp = self.temperature.copy()
-      self.altered_elevation = self.elevation.copy()
+      self.altered_temp = self.temperature.astype(dtype=np.int64,casting="unsafe")
+      self.altered_elevation = self.elevation.astype(dtype=np.int64,casting="unsafe")
    def isolate_and_divide(self,array,shift):
       """Isolates a byte (setting other bytes to zero), and divides it by SHEET_SIZE_FRACTION"""
       output = array.copy()
@@ -36,21 +36,21 @@ class BiomeMap(object):
    def dynamic_range(self,x):
       """Scale and clamp a floating point value between -1.0 and 1.0 to be between -SHEET_SIZE and SHEET_SIZE"""
       return min(ColorMap.SHEET_SIZE,max(-ColorMap.SHEET_SIZE, #clamp
-         int(x * 256 * SHEET_SIZE_FRACTION)
+         int(x * ColorMap.SHEET_SIZE)
       ))
    def stamp(self,sealevel,templevel,colormap,surface):
       """Writes the planet sprite onto the given surface. Sealevel and templevel have dynamic range from -1.0 to 1.0"""
-      sealevel = dynamic_range(sealevel)
-      templevel = dynamic_range(templevel)
+      sealevel = self.dynamic_range(sealevel)
+      templevel = self.dynamic_range(templevel)
       data = surfarray.pixels2d(surface)
       np.copyto(data,self.alpha) # A clean slate
       # Temperature
-      np.sub(self.temperature,templevel,out=self.altered_temp)
-      np.clip(self.altered_temp,0,ColorMap.SHEET_SIZE,out=self.altered_temp)
-      np.sub(self.elevation,sealevel,out=self.altered_elevation)
-      np.clip(self.altered_elevation,0,ColorMap.SHEET_SIZE,out=self.altered_elevation)
+      np.subtract(self.temperature,templevel,out=self.altered_temp)
+      np.clip(self.altered_temp,0,ColorMap.SHEET_SIZE-1,out=self.altered_temp)
+      np.subtract(self.elevation,sealevel,out=self.altered_elevation)
+      np.clip(self.altered_elevation,0,ColorMap.SHEET_SIZE-1,out=self.altered_elevation)
       self.scratch[:] = colormap.colors[self.altered_elevation,self.altered_temp]
-      np.bitwise_or(self.scratch,data,out=data)
+      np.bitwise_or(self.scratch.astype(dtype=np.uint32,casting="unsafe",copy=False),data,out=data)
    def make_surface(self):
       """Makes a surface ready to receive pixel data from stamp."""
       size = self.alpha.shape
@@ -58,7 +58,7 @@ class BiomeMap(object):
       
 class PlanetSprite(object):
    CANVAS_SIZE = (256,256)
-   def __init__(self,pos,biomemap,colormap,scale=1,omega=1,theta=0):
+   def __init__(self,pos,biomemap,colormap,scale=1,omega=0,theta=0):
       self.pos = pos
       self.theta = theta
       self.omega = omega
@@ -94,21 +94,13 @@ class PlanetSprite(object):
       )
       surface.blit(night,night_pos,pygame.Rect(day.get_width()//2,0,day.get_width()//2,day.get_height()) )
 
-def load(tfile,pfile):
-   terrain = pygame.image.load(tfile).convert_alpha()
-   biomes = pygame.image.load(pfile).convert_alpha()
-   BiomeMap(biomes,biomes.get_rect())
-   terrain_map = surfarray.array3d(terrain)
-   biome_array = surfarray.array2d(biomes)
-   # Disassemble
-   shifts = biomes.get_shifts()
-   r = np.bitwise_and(np.right_shift(biome_array,shifts[0]),0xff) // 2
-   g = np.bitwise_and(np.right_shift(biome_array,shifts[1]),0xff) // 2
-   b = np.bitwise_and(np.right_shift(biome_array,shifts[2]),0xff) // 2
-   a = np.bitwise_and(np.right_shift(biome_array,shifts[3]),0xff)
-   print(r[0,0],b[0,0],g[0,0],a[0,0],biome_array[128,128])
-   planet_surface = pygame.Surface(biomes.get_rect().size,SRCALPHA,32)
-   return terrain_map,planet_surface,a,g,r
+def load(tfile,pfile,cindex,bindex):
+   color_sheet = pygame.image.load(tfile).convert_alpha()
+   biomes_sheet = pygame.image.load(pfile).convert_alpha()
+   cmap = ColorMap(color_sheet,pygame.Rect(cindex*ColorMap.SHEET_SIZE,0,ColorMap.SHEET_SIZE,ColorMap.SHEET_SIZE))
+   bmap = BiomeMap(biomes_sheet,pygame.Rect(bindex*160,0,160,160))
+   psprite = PlanetSprite((128,128),bmap,cmap)
+   return psprite
    
 def update(templevel,sealevel,temperatures,elevations,alpha,terrain_map,planet_surface):
    shifts = planet_surface.get_shifts()
@@ -120,7 +112,7 @@ def update(templevel,sealevel,temperatures,elevations,alpha,terrain_map,planet_s
    d = c | (np.array(alpha,dtype=np.uint32) << shifts[3])
    planet_array[:] = np.array(d,dtype=np.uint32)
 
-terrain_map,planet_surface,alpha,temperatures,elevations = load("terrain.png","planet.png")
+psprite = load("terrain.png","planet.png",0,4)
 clock = pygame.time.Clock()
 
 while True:
@@ -134,12 +126,13 @@ while True:
             pygame.quit()
             exit()
          if event.key == K_SPACE:
-            terrain_map,planet_surface,alpha,temperatures,elevations = load("terrain.png","planet.png")
+            psprite = load("terrain.png","planet.png",0,0)
+   psprite.tick(dt)
    sealevel,templevel = pygame.mouse.get_pos()
-   templevel = templevel - 128
-   sealevel = sealevel - 128
+   templevel = (templevel - 128) / 128
+   sealevel = (sealevel - 128) / 128
    screen.fill((0,0,0))
-   update(templevel,sealevel,temperatures,elevations,alpha,terrain_map,planet_surface)
-   screen.blit(planet_surface,(0,0))
+   psprite.set_parameters(sealevel,templevel) 
+   psprite.draw(screen)
    pygame.display.flip()
    
