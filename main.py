dd77cf75146a894b51ba4bf8c242a90b07f4ea76
user <user@example.com>
2020-04-18 23:31:07 -0500

    main.py implements physics

--- main.py
+++ main.py
@@ -5,13 +5,94 @@ import numpy as np
 
 import planet
 
+class Gravitator(object):
+   def __init__(self,universe,pos,mass,radius):
+      universe.gravitators.append(self)
+      universe.sprites.append(self) # debug drawing
+      self.pos = np.array(pos,dtype=np.float64) # convert to numpy array for that sweet operator overloading
+      self.mass = mass
+      self.radius = radius
+   def draw(self,screen):
+      return pygame.draw.circle(
+         screen,
+         (min(255,max(0,int(self.mass))),
+          min(255,max(0,int(self.mass*255))),
+          min(255,max(0,int(np.log(self.mass))))),
+         (int(self.pos[0]),int(self.pos[1])),
+         int(self.radius),
+         2,
+      ) # return impacted pixels
+
 class Player(object):
    RADIUS = 16 # radius for physics purposes
-   def __init__(self,universe,sprite,pos,vel=(0,0)):
+   G = 1 # The strength of the force of gravity
+   THRUST = 0.001
+   BOUNCE_DAMP = 0.1
+   def __init__(self,universe,sprite,pos,angle=0,vel=(0,0)):
       self.universe = universe
       self.sprite = sprite
-      self.pos = np.array(pos,dtype=np.float64) # convert to numpy array for that sweet operator overloading
+      self.pos = np.array(pos,dtype=np.float64) 
       self.vel = np.array(vel,dtype=np.float64)
+      self.acc = np.array((0,0),dtype=np.float64) # we want to keep around last frame's acceleration for velocity verlet
+      self.angle = angle
+      universe.sprites.append(self)
+      universe.things.append(self)
+      universe.player = self
+   def wrap(self):
+      '''Enforce toroidal geometry'''
+      if self.pos[1] > self.universe.wrapping_rect.bottom + self.RADIUS:
+         self.pos[1] = self.universe.wrapping_rect.top-self.RADIUS
+      elif self.pos[1] < self.universe.wrapping_rect.top-self.RADIUS:
+         self.pos[1] = self.universe.wrapping_rect.bottom + self.RADIUS
+      
+      if self.pos[0] > self.universe.wrapping_rect.right + self.RADIUS:
+         self.pos[0] = self.universe.wrapping_rect.left-self.RADIUS
+      elif self.pos[0] < self.universe.wrapping_rect.left-self.RADIUS:
+         self.pos[0] = self.universe.wrapping_rect.right + self.RADIUS
+   def gravity_at(self,pos):
+      '''Computes the acceleration due to gravity due to a body.'''
+      g = np.array((0.0,0.0))
+      for gravitator in self.universe.gravitators:
+         r = gravitator.pos - self.pos
+         r_mag_raised_to_three = np.power(np.sum(np.square(r)),3/2)
+         g += self.G*gravitator.mass*r / r_mag_raised_to_three
+      return g
+   def thrust(self):
+      '''Computes how much we should be thrusting based on our controls.'''
+      return np.array((
+         self.THRUST*np.cos(self.angle),
+         self.THRUST*np.sin(self.angle),
+      )) if pygame.mouse.get_pressed()[0] else np.array((0.0,0.0))
+   def collide(self):
+      '''Check if we're inside a planet, and get us out if we are.'''
+      for gravitator in self.universe.gravitators:
+         r = gravitator.pos - self.pos
+         r_mag = np.sqrt(np.sum(np.square(r)))
+         if r_mag < self.RADIUS+gravitator.radius:
+            r_hat = r/r_mag
+            v_proj_r_hat = np.sum(self.vel*r_hat)*r_hat # project velocity on to radial vector
+            self.vel -= (2-self.BOUNCE_DAMP)*v_proj_r_hat # elastic colission
+            self.pos = gravitator.pos - r_hat*(self.RADIUS+gravitator.radius) # put us back on the surface
+   def tick(self,dt):
+      # Point at the mouse
+      delta_to_mouse = np.array(pygame.mouse.get_pos(),dtype=np.float64) - self.pos
+      self.angle = np.arctan2(delta_to_mouse[1],delta_to_mouse[0]) 
+      # Collide with planets
+      self.collide()
+      # Update velocity
+      self.pos = self.pos + self.vel*dt + 0.5*self.acc*(dt*dt)
+      new_acc = self.gravity_at(self.pos) + self.thrust()
+      self.vel = self.vel + 0.5*dt*(self.acc + new_acc)
+      self.acc = new_acc # save acceleration for next frame
+      # Enforce toroidal geometry
+      self.wrap()
+   def draw(self,screen):
+      rotated_sprite = pygame.transform.rotozoom(self.sprite,-self.angle*(180/np.pi)-90,1)
+      pos = (int(self.pos[0]-rotated_sprite.get_width()/2),int(self.pos[1]-rotated_sprite.get_height()/2))
+      return screen.blit(
+         rotated_sprite,
+         pos
+      ) # Returns impacted pixels
 
 class Universe(object):
    def __init__(self,background,wrapping_rect=None):
@@ -19,6 +100,7 @@ class Universe(object):
       self.wrapping_rect = wrapping_rect or background.get_rect() # if wrapping_rect is null, use the background rect
       self.sprites = [] # things that need to have draw(screen) called on them
       self.things = [] # things that need to have tick(dt) called on them
+      self.gravitators = [] # things that create gravitational fields      
       self.dirty_rects = [background.get_rect()] # patches of the background that will need to be redrawn
       self.player = None
       self.planets = []
@@ -26,13 +108,19 @@ class Universe(object):
       bg_rect = self.background.get_rect()
       for rect in self.dirty_rects:
          screen.blit(self.background,rect.topleft,rect.clip(bg_rect))
+      touched_rects = self.dirty_rects
+      self.dirty_rects = []
       for sprite in self.sprites:
          rect = sprite.draw(screen)
          if rect: # If the draw function has created a dirty rect
             self.dirty_rects.append(rect)
+            touched_rects.append(rect)
+      return touched_rects
    def tick(self,dt):
       for thing in self.things:
          thing.tick(dt)
+   def populate(self):
+      Gravitator(self,(500,500),100,80)
 
 if __name__ == "__main__":
    pygame.init()
@@ -40,6 +128,8 @@ if __name__ == "__main__":
    screen = pygame.display.set_mode(screen_size)
    clock = pygame.time.Clock()
    universe = Universe(pygame.image.load("img/nebula.jpg").convert())
+   Player(universe,pygame.image.load("img/sprite_grave.png").convert_alpha(),(500,300),0,(0.7,0))
+   universe.populate()
    while True:
         dt = clock.tick(60)  # If we go faster than 60fps, stop and wait.
         for event in pygame.event.get():  # Get everything that's happening
@@ -50,5 +140,4 @@ if __name__ == "__main__":
                 pygame.quit()
                 exit()
         universe.tick(dt)
-        universe.draw(screen)
-        pygame.display.flip()
+        pygame.display.update(universe.draw(screen)) # Only push out the stuff we changed to the OS
