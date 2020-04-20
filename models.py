1e443f3bc2e22400a33498d5bbe0115a3664798d
user <user@example.com>
2020-04-20 10:38:28 -0500

    Refactored models.py

--- models.py
+++ models.py
@@ -1,57 +1,70 @@
 #!/usr/bin/env python
 import numpy as np
 
-class SineModel(object):
-   SPEED = 0.002 # average change to parameters each milisecond
-   PULL_TO_CENTER = 0.0002
-   def __init__(self,string = "Hi",stringcolour = (255,255,255)):
-      # Choose random starting values
-      self.string = string
-      self.stringColour = stringcolour
-      self.sea = np.random.uniform(-0.8,0.8)
-      self.temp = np.random.uniform(-0.8,0.8)
-      self.pop = np.random.uniform(0.0,1.0)
-      self.tech = np.random.uniform(0.0,1.0)
-      self.maxPop = 1
-      self.ticks = 0
-      self.isEnlightened = False
+class Model(object):
+   DEFAULT_SPEED = 0.001 # dt multiplier
+   def __init__(self,sea=0.0,temp=0.0,pop=0.0,tech=0.0,speed=1):
+      self.sea = sea
+      self.temp = temp
+      self.pop = pop
+      self.tech = tech
+      self.speed = speed*self.DEFAULT_SPEED
+      self.enlightened = False
+   def tick(self,dt):
+      pass
 
-   def isEnlightened(self):
-        return self.isEnlightened
+class SineModel(Model):
+   SEA_SPEED = 0.1
+   TEMP_SPEED = 0.1
+   POP_SPEED = 0.01
+   TECH_SPEED = 0.01
+   
+   # The range of temp and sea values required for pop growth
+   GOOD_TEMP_ZONE = 0.3
+   GOOD_SEA_ZONE = 0.3
+   GOOD_POP_ZONE = 0.25 # width around 0.5 pop for tech growth
+   def __init__(self):
+      # Choose random starting values
+      super().__init__(
+         sea=np.random.uniform(-self.GOOD_SEA_ZONE,self.GOOD_SEA_ZONE),
+         temp=np.random.uniform(-self.GOOD_TEMP_ZONE,self.GOOD_TEMP_ZONE),
+         pop=0.1,tech=0.0
+      )
 
    def tick(self,dt):
-        if not self.isEnlightened:
-            self.ticks += 1*dt
-            self.temp += -self.sea*dt*0.0001
-            self.temp = np.clip(self.temp,-1,1)
-            self.sea += self.temp*dt*0.0001
-            self.sea = np.clip(self.sea, -1, 1)
-            self.pop = np.clip(self.pop, 0, 1)
-            self.tech = np.clip(self.tech, 0, 1)
-            if -0.3 < self.temp < 0.3 and -0.3 < self.sea < 0.3:
-                 self.pop += 0.0001
-            else:
-                 self.pop -= 0.0001
-            if 0.3 < self.pop < 0.8:
-                self.tech += 0.0001 - (0.55-self.pop)*0.0001
-            #Yeah this fixes a bug where there's only 1 guy and he keeps rebuilding society lol
-            if self.pop <= 0.001:
-                self.pop = 0
-            if self.pop == 0:
-                self.tech = 0
-        if self.tech >= 1:
-            self.isEnlightened = True
-            self.temp = self.temp
+      ds = self.speed * dt
+      if not self.enlightened:
+         self.temp += -self.sea*ds*self.TEMP_SPEED
+         self.sea += self.temp*ds*self.SEA_SPEED
+         if -self.GOOD_TEMP_ZONE < self.temp < self.GOOD_TEMP_ZONE and \
+            -self.GOOD_SEA_ZONE < self.sea < self.GOOD_SEA_ZONE:
+            self.pop += self.POP_SPEED*ds
+         else:
+             self.pop -= self.POP_SPEED*ds
+         if 0.5-self.GOOD_POP_ZONE < self.pop < 0.5+self.GOOD_POP_ZONE:
+             self.tech += (1-0.5+self.pop)*ds*self.TECH_SPEED
+         #Yeah this fixes a bug where there's only 1 guy and he keeps rebuilding society lol
+         if self.pop <= 0.001:
+             self.pop = 0
+         if self.pop == 0:
+             self.tech = 0
+      if self.tech >= 1:
+         self.enlightened = True
+      self.temp = np.clip(self.temp,-1,1)
+      self.sea = np.clip(self.sea, -1, 1)
+      self.pop = np.clip(self.pop, 0, 1)
+      self.tech = np.clip(self.tech, 0, 1)
 
-class StochasticModel(object):
+class StochasticModel(Model):
    SPEED = 0.002 # average change to parameters each milisecond
    PULL_TO_CENTER = 0.0002
    def __init__(self):
-      # Choose random starting values
-      self.sea = np.random.uniform(-1.0,1.0)
-      self.temp = np.random.uniform(-1.0,1.0)
-      self.pop = np.random.uniform(0.0,1.0)
-      self.tech = np.random.uniform(0.0,1.0)
+      super().__init__(
+         np.random.uniform(-1.0,1.0),
+         np.random.uniform(-1.0,1.0),
+         np.random.uniform(0.0,1.0),
+         np.random.uniform(0.0,1.0)
+      )
    def random_delta(self,dt,value,off_center):
       step = np.random.uniform(-1.0,1.0)*self.SPEED*dt
       restoration = - self.PULL_TO_CENTER*off_center*dt
